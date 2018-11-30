87256e2942224746ccf9866ebe5a691b8851b04a
MifReal <MifReal@list.ru>
2018-12-01 01:20:18 +0400

    simple server

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import os
 import telegram
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
 
-bot = telegram.Bot(token=os.environ.get('BOT_TOKEN', ''))
-print('a')
 
-def webhook(request):
-    print('b')
-    if request.method == "POST":
-        print('c')
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        request = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
-    return "ok"
+
+if __name__ == '__main__':
+    bot = telegram.Bot(token=os.environ.get('BOT_TOKEN', ''))
+    httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
