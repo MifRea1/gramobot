2847ea8b558597fa1374ffae6af46077d8b4ee69
MifReal <MifReal@list.ru>
2018-12-01 00:33:07 +0400

    debug prints

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import telegram
 
 bot = telegram.Bot(token=os.environ.get('BOT_TOKEN', ''))
-
+print('a')
 
 def webhook(request):
+    print('b')
     if request.method == "POST":
+        print('c')
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
